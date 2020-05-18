9cb542a158c972ff91833b583144daac0bf28722
Rodrigo da Silva <rodrigo@rodrigoads.com>
2020-05-18 16:05:57 -0300

    Create the table

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
 var field = [];
+var tableField;
+var bombsNumber;
 
-function makerField(){
-    for(var c = 0; c <= 8; c++){
 
+
+function makerField(columms, rows, bombs) {
+    tableField = '<table style="width:30%">';
+
+    var celNumber = 1;
+    for (var c = 0; c < columms; c++) {
+        field[c] = [];
+        tableField = tableField + "<tr>";
+        for (var r = 0; r < rows; r++) {
+            field[c].push({
+                "coluna": c,
+                "linha": r,
+                "celNumber": celNumber
+            });
+            tableField = tableField + "<td>" + c + "/" + r + "<br>" + celNumber + "</td>";
+            celNumber++;
+        }
+        tableField = tableField + "</tr>";
     }
+    tableField = tableField + "</table>";
+
+    document.getElementById("minefield").innerHTML = tableField;
 }
